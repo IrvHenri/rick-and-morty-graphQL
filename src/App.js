30cea4664f4727268d0e16243446c2d51c539e62
Irving Henriquez <henriquezirv@gmail.com>
2021-08-11 20:11:45 -0400

    set up apollo client and providor to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
-
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+import RickAndMortyContainer from "./containers/RickAndMortyContainer";
 function App() {
-  return <div className="App"></div>;
+  const client = new ApolloClient({
+    uri: "https://rickandmortyapi.com/graphql",
+  });
+  return (
+    <div className="App">
+      <ApolloProvider client={client}>
+        <main>
+          <RickAndMortyContainer />
+        </main>
+      </ApolloProvider>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/containers/RickAndMortyContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const RickAndMortyContainer = () => {
+  return (
+    <div>
+      <p>Rick and Mort </p>
+    </div>
+  )
+}
+
+export default RickAndMortyContainer
